7390466243937204473f48f1f9a1df9f5ad9759d
Z <z@apiary.io>
2015-12-29 15:17:22 +0100

    Provide access to parse result refract

--- spec/linter-helper-spec.js
+++ spec/linter-helper-spec.js
@@ -4,12 +4,14 @@ const minim = require('minim').namespace()
 
 import {
   resolutionType,
+  resolutionRange,
   createResolution
 }
 from '../lib/helpers.js'
 
 import AnnotationRefractFixture from './fixtures/annotation.refract.json'
 import AnnotationResolutionFixture from './fixtures/annotation.resolution.json'
+import ComplexSourceMapFixture from './fixtures/complexsourcemap.refract.json'
 
 describe('API Blueprint linter helpers', () => {
 
@@ -26,11 +28,22 @@ describe('API Blueprint linter helpers', () => {
     })
   })
 
+  describe('Resolution range helper', () => {
+
+    it ('computes the correct range', () => {
+      let annotation = minim.fromRefract(ComplexSourceMapFixture)
+      let sourceMap = annotation.attributes.get('sourceMap')
+      let range = resolutionRange(sourceMap)
+      expect(range.length).toEqual([[3,4], [5,5]])
+    })
+
+  })
+
   describe('Create resolution helper', () => {
 
     it('creates resolution from a warning annotation', () => {
       let annotation = minim.fromRefract(AnnotationRefractFixture)
-      let resolution = createResolution('', annotation)
+      let resolution = createResolution('', '', annotation)
       expect(resolution).toEqual(AnnotationResolutionFixture)
     })
 
