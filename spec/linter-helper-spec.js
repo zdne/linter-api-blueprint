46ab0f2e2598fe6f15fb0a6f52c254ac12e5a5c1
Z <z@apiary.io>
2016-01-04 14:56:45 +0100

    Migrate to refract-query

--- spec/linter-helper-spec.js
+++ spec/linter-helper-spec.js
@@ -1,10 +1,8 @@
 'use babel'
 
-const minim = require('minim').namespace()
-
 import fs from 'fs'
 import path from 'path'
-
+import query from 'refract-query'
 import {
   lineEndingsIndices,
   resolutionType,
@@ -29,12 +27,12 @@ describe('API Blueprint linter helpers', () => {
   describe('Resolution type helper', () => {
 
     it('recognizes the warning source annotation class', () => {
-      let type = resolutionType(minim.toElement(['warning']))
+      let type = resolutionType(['warning'])
       expect(type).toEqual('Warning')
     })
 
     it('recognizes the error source annotation class', () => {
-      let type = resolutionType(minim.toElement(['red', 'green', 'error']))
+      let type = resolutionType(['red', 'green', 'error'])
       expect(type).toEqual('Error')
     })
   })
@@ -59,9 +57,8 @@ describe('API Blueprint linter helpers', () => {
 
     it('computes the correct range', () => {
       let endingsIndices = lineEndingsIndices(textBuffer)
-      let annotation = minim.fromRefract(ComplexSourceMapRefractFixture)
-      let sourceMap = annotation.attributes.get('sourceMap')
-      let range = resolutionRange(endingsIndices, sourceMap)
+      const sourceMap = ComplexSourceMapRefractFixture.attributes.sourceMap
+      const range = resolutionRange(endingsIndices, sourceMap)
       expect(range).toEqual([
         [3, 4],
         [5, 5]
@@ -74,33 +71,28 @@ describe('API Blueprint linter helpers', () => {
     let textBuffer
 
     beforeEach(() => {
-      let fixturePath = path.join(__dirname, InvalidBlueprintFixturePath)
+      const fixturePath = path.join(__dirname, InvalidBlueprintFixturePath)
       textBuffer = fs.readFileSync(fixturePath, 'utf8')
     })
 
     it('creates the resolution from a warning annotation', () => {
-      let annotation = minim.fromRefract(AnnotationRefractFixture)
-      let resolution = createResolution('', '', annotation)
+      const resolution = createResolution('', '', AnnotationRefractFixture)
       expect(resolution).toEqual(AnnotationResolutionFixture)
     })
 
     it('creates the resolution from an error annotation', () => {
-      let annotation = minim.fromRefract(InvalidRefractFixture)
-      let endingsIndices = lineEndingsIndices(textBuffer)
-
-      let resolution = createResolution(endingsIndices, InvalidBlueprintFixturePath, annotation)
+      const endingsIndices = lineEndingsIndices(textBuffer)
+      const resolution = createResolution(endingsIndices, InvalidBlueprintFixturePath, InvalidRefractFixture)
       expect(resolution).toEqual(InvalidResolutionFixture)
     })
 
     it('creates resolution from parser result', () => {
-      let parseResult = minim.fromRefract(InvalidParseResultFixture)
-      let endingsIndices = lineEndingsIndices(textBuffer)
-
+      const endingsIndices = lineEndingsIndices(textBuffer)
       var resolutions = []
-      var annotations = parseResult.content.filter(item => item.element === 'annotation')
+      const annotations = query(InvalidParseResultFixture, {element: 'annotation'});
       annotations.forEach(function(annotation) {
-        console.log(`\nannotation: ${JSON.stringify(annotation, ' ', 2)}`)
-        resolutions.push(createResolution(endingsIndices, InvalidBlueprintFixturePath, minim.fromRefract(annotation)))
+        //console.log(`\nannotation: ${JSON.stringify(annotation, ' ', 2)}`)
+        resolutions.push(createResolution(endingsIndices, InvalidBlueprintFixturePath, annotation))
       })
       expect(resolutions.length).toEqual(2)
     })
